7fbb00af3315a20f206ffc49e9cdabfb9a6bcb34
Aman parmar <amanpagal9093@gmail.com>
2023-05-03 15:30:51 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,39 @@
+       
+
+let firstName="its me";
+let LastName=" Aman parmar";
+let middle_name=" Welcome to my Shubh Mangal website";
+ 
+
+
+ var time=prompt("hey what is time:")
+ if(time>4 && time<12){
+        alert("Good morning Sir");
+        alert(firstName+LastName+middle_name);
+ }
+ else if(12<time<16) {
+        alert("Good afternoon Sir");
+        alert(firstName+LastName+middle_name);
+ }
+ else {
+       alert("Good evening Sir");
+       alert(firstName+LastName+middle_name);
+}
+
+
+
+document.getElementById("gree").innerHTML =
+"Thanks";
+
+document.getElementById("greed").innerHTML =
+"For Visit";
+
+
+function myFunction() {
+       document.getElementById("by1").innerHTML =
+        "We Will Meet";
+       document.getElementById("by2").innerHTML = "Next Time?";
+     }
+
+
+
